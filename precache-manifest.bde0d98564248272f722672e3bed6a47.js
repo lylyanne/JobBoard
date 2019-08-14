a8794bf965dd59fddf692d1985e0df9a37331c44
Felicia Wang <felis_3007@yahoo.com>
2019-08-14 15:48:21 -0700

    Adding build folder

---
+++ precache-manifest.bde0d98564248272f722672e3bed6a47.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e79caafe9ed0d09019aa",
+    "url": "/static/js/main.bd6624e9.chunk.js"
+  },
+  {
+    "revision": "de3ee44c66eef8724a0d",
+    "url": "/static/js/2.57845d61.chunk.js"
+  },
+  {
+    "revision": "2428b99d757a644e0cfe7f10dd526a92",
+    "url": "/index.html"
+  }
+];
